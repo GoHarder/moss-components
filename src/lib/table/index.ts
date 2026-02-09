e1afbff7181f003a7fdc5b5f34e819a04d4ca9da
Gregory Harder <goharder83@gmail.com>
2026-02-09 15:20:24 -0600

    Removed table so it can build.

--- src/lib/table/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Cell } from './cell.svelte';
-export { default as Nav } from './nav.svelte';
-export { default as Row } from './row.svelte';
-export { default as Table } from './table.svelte';
